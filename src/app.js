7d419e077e11f3720efb02f579c63678379cc353
Matthew Ceroni <matthew.ceroni@dat.com>
2020-09-04 12:25:01 -0700

    Update messagse

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const PORT = 8080;
 
 // create a route for the app
 app.get('/', (req, res) => {
-  res.send('Hello. Breast cancer death rates declined 40% from 1989 to 2016 among women.<p><a href="https://www.nationalbreastcancer.org/early-detection-of-breast-cancer/">The progress is attributed to improvements in early detection.</a>');
+  res.send('Hello. Niners were robbed in the SB last year.</a>');
 });
 
 // make the server listen to requests
